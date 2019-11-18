2d0c77d30fc5dd8e7494b25de4768023c318ec91
Phurich Nilvisut <5910210688@psu.ac.th>
2019-11-18 23:27:40 +0700

    Update index.html and another JS

---
+++ js/DT8540_else.js
@@ -0,0 +1,173 @@
+if (hist[288] <= 0) {
+    if (hist[202] <= 0.024) {
+        if (hist[238] <= 0) {
+            if (hist[162] <= 0.027) {
+                if (hist[272] <= 0) {
+                    if (hist[138] <= 0.011) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                } else {
+                    if (hist[156] <= 0.316) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                }
+            } else {
+                if (hist[246] <= 0) {
+                    if (hist[262] <= 0) {
+                        classes = 'Uninfected';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                } else {
+                    if (hist[174] <= 0.002) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                }
+            }
+        } else {
+            if (hist[236] <= 0.011) {
+                if (hist[304] <= 0) {
+                    if (hist[156] <= 0.024) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                } else {
+                    if (hist[0] <= 0.15) {
+                        classes = 'Uninfected';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                }
+            } else {
+                if (hist[266] <= 0) {
+                    if (hist[158] <= 0.004) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                } else {
+                    classes = 'Parasitized';
+                }
+            }
+        }
+    } else {
+        if (hist[292] <= 0) {
+            if (hist[322] <= 0) {
+                if (hist[98] <= 0.001) {
+                    if (hist[354] <= 0) {
+                        classes = 'Uninfected';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                } else {
+                    if (hist[114] <= 0.009) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                }
+            } else {
+                classes = 'Parasitized';
+            }
+        } else {
+            if (hist[270] <= 0.005) {
+                if (hist[242] <= 0.193) {
+                    if (hist[160] <= 0.145) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                } else {
+                    classes = 'Uninfected';
+                }
+            } else {
+                if (hist[292] <= 0.002) {
+                    classes = 'Uninfected';
+                } else {
+                    classes = 'Parasitized';
+                }
+            }
+        }
+    }
+} else {
+    if (hist[246] <= 0.028) {
+        if (hist[48] <= 0.152) {
+            if (hist[120] <= 0.782) {
+                if (hist[32] <= 0.091) {
+                    if (hist[38] <= 0.451) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                } else {
+                    if (hist[308] <= 0.001) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                }
+            } else {
+                classes = 'Uninfected';
+            }
+        } else {
+            if (hist[352] <= 0.003) {
+                if (hist[60] <= 0.015) {
+                    if (hist[98] <= 0.002) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                } else {
+                    if (hist[168] <= 0.065) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                }
+            } else {
+                classes = 'Uninfected';
+            }
+        }
+    } else {
+        if (hist[358]) {
+            if (hist[348] <= 0.001) {
+                if (hist[388] <= 0) {
+                    if (hist[146] <= 0.025) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Uninfected';
+                    }
+                } else {
+                    classes = 'Parasitized';
+                }
+            } else {
+                if (hist[374] <= 0.002) {
+                    classes = 'Parasitized';
+                } else {
+                    classes = 'Uninfected';
+                }
+            }
+        } else {
+            if (hist[172] <= 0) {
+                classes = 'Uninfected';
+            } else {
+                if (hist[276] <= 0) {
+                    classes = 'Uninfected';
+                } else {
+                    if (hist[250] <= 0.021) {
+                        classes = 'Parasitized';
+                    } else {
+                        classes = 'Parasitized';
+                    }
+                }
+            }
+        }
+    }
+}
